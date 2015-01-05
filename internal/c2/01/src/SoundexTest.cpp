1cc4395e3c766fbba1e7262ee861a9955a86816f
Telford Berkey <tberkey@4qbase.com>
2015-01-05 16:15:49 -0500

    Updated with encoding.

--- internal/c2/01/src/SoundexTest.cpp
+++ internal/c2/01/src/SoundexTest.cpp
@@ -1,10 +1,21 @@
 #include <gmock/gmock.h>
 
+using ::testing::Eq;
+
 class Soundex
 {
+    public:
+        std::string encode(const std::string& word) const
+        {
+            return word;
+        }
 };
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
 { 
-   Soundex soundex;   
+   Soundex soundex;
+
+   auto encoded = soundex.encode("A");
+
+   ASSERT_THAT(encoded, Eq("A"));
 }
